3c30c6836613cec45e9931bba71ed1f6587f32de
Shubham Patel <ptlshubham@gmail.com>
2026-05-06 00:47:27 +0530

    refactor: remove unused service configuration files

--- js/demos/demo-barber.js
+++
@@ -1,5 +0,0 @@
-/*
-Name: 			Barber
-Written by: 	Okler Themes - (http://www.okler.net)
-Theme Version:	13.0.0
-*/
--- js/demos/demo-renewable-energy.js
+++
@@ -1,5 +0,0 @@
-/*
-Name:           Demo Renewable Energy
-Written by:     Okler Themes - (http://www.okler.net)
-Theme Version:  13.0.0
-*/
